1e680ec8c6e647cda7e5b4c775b2ac595c6a6ee3
Saman Alishiri Shahrbabak <samanalishiri@gmail.com>
2019-01-27 15:08:31 +0330

    refactor directory

--- ejb-tutorial/step_001_simple_crud/module-ejb/src/main/java/com/saman/tutorial/javaee/ejb/domain/AbstractEntity.java
+++ ejb-tutorial/step_001_simple_crud/module-ejb/src/main/java/com/saman/tutorial/javaee/ejb/domain/AbstractEntity.java
@@ -1,7 +1,9 @@
 package com.saman.tutorial.javaee.ejb.domain;
 
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public abstract class AbstractEntity<T> implements Serializable {
 
     public abstract T getId();
--- ejb-tutorial/step_001_simple_crud/module-ejb/src/main/java/com/saman/tutorial/javaee/ejb/repository/UserRepository.java
+++ ejb-tutorial/step_001_simple_crud/module-ejb/src/main/java/com/saman/tutorial/javaee/ejb/repository/UserRepository.java
@@ -3,13 +3,8 @@ package com.saman.tutorial.javaee.ejb.repository;
 import com.saman.tutorial.javaee.ejb.domain.UserEntity;
 
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
-import javax.persistence.PersistenceUnit;
-import javax.transaction.Transactional;
 
-@Stateless
+@Stateless(mappedName = "userRepository")
 public class UserRepository extends AbstractRepository<Long, UserEntity> implements IUserRepository {
 
 }
